967508858462ecccfaa6705b35708aaa7e39bc87
Lukas Mikulenas <lmikulenas@ucvts.org>
2021-06-10 10:13:39 -0400

    completed Rook

--- src/Rook.java
+++ src/Rook.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Rook {
+public class Rook extends Parent {
+    public Rook(int row, int col, String color) {
+        super(row, col, color);
+    }
+    public boolean mayMove(int row, int col) {
+        for (Chess piece : pieces) {
+            if (piece.findRow() == row && piece.findColumn() == col) return false;
+        } if (row > 7 || row < 0) return false;
+        if (col > 7 || col < 0) return false;
+        if (row == this.findRow() && col != this.findColumn()) return true;
+        return (row != this.findRow() && col == this.findColumn());
+    }
+    public boolean canDefeat(Chess piece) {
+        if (this.findColumn() == piece.findColumn() && this.findRow() != piece.findRow() && !piece.findColor().equals(this.findColor())) return true;
+        return (this.findColumn() != piece.findColumn() && this.findRow() == piece.findRow() && !piece.findColor().equals(this.findColor()));
+    }
 }
